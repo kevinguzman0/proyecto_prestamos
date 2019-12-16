a7135e568e957828ef441163eda7b7a1fc9db046
kevinguzman0 <53454298+kevinguzman0@users.noreply.github.com>
2019-12-16 15:41:50 -0600

    mejorLogin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/logueo';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
