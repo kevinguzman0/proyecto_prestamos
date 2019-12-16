67e777f61e056ebd613e05966ef490cb110a5f21
kevinguzman0 <53454298+kevinguzman0@users.noreply.github.com>
2019-12-16 15:27:47 -0600

    sin inicio

--- routes/web.php
+++ routes/web.php
@@ -16,77 +16,7 @@ use Illuminate\Database\Eloquent\Model;
 
 Auth::routes();
 
-Route::get('/tabla1', function () {
-
-	$tablaConfiguracion = new App\TablaConfiguraciones();
-	$tablaConfiguracion->nombreConfiguracion = 'configuracion #1';
-	$tablaConfiguracion->valorConfiguracion = 'valor #1';
-	$tablaConfiguracion->descripcionConfiguracion = 'descripcion #1';
-	$tablaConfiguracion->save();
-    return 'Registro de tabla de configuraciones creado...';
-});
-
-Route::get('/tabla2', function () {
-
-	$perfilUsuario = new App\PerfilesUsuario();
-	$perfilUsuario->nombrePerfil = 'perfil #1';
-	$perfilUsuario->descripcionPerfil = 'descripcion #1';
-	$perfilUsuario->save();
-    return 'Registro de perfil de usuario creado...';
-});
-
-Route::get('/tabla3', function () {
-
-	$estadoSolicitud = new App\EstadoSolicitudes();
-	$estadoSolicitud->nombreEstado = 'estado #1';
-	$estadoSolicitud->descripcionEstado = 'descripcion #1';
-	$estadoSolicitud->save();
-    return 'egistro de estado de solicitud creado...';
-});
-
-Route::get('/tabla4', function () {
-
-    $usuario = new App\Usuarios();
-    $usuario->idPerfilUsuario = '1';
-    $usuario->cedula = '1024551252';
-    $usuario->nombres = 'jhon';
-    $usuario->apellidos = '1024551252';
-    $usuario->foto = '1222';
-    $usuario->email = 'jhon@gmail.com';
-    $usuario->telefono1 = '13883293';
-    $usuario->telefono2 = '10737383';
-    $usuario->fechaNacimiento = '2001/04/11';
-    $usuario->direccion = 'cll 34 N 34-34';
-    $usuario->barrio = 'madelena';
-    $usuario->ciudad = 'bogota';
-    $usuario->areaTrabajo = 'administrativa';
-    $usuario->cargoTrabajo = 'sub-gerente';
-    $usuario->save();
-    return view('inicio');
-});
-
-Route::get('/tabla5', function () {
-
-	$solicitud = new App\Solicitudes();
-	$solicitud->monto='50000';
-	$solicitud->plazo='15';
-	$solicitud->cuota15='5000000.50';
-	$solicitud->cuota30='1000000.10';
-	$solicitud->tasa='45.5';
-	$solicitud->idEstadoSolicitud='1';
-	$solicitud->idCliente='1';
-	$solicitud->save();
-	
-    return 'Solicitud aceptada XD';
-});
-
-/*
 Route::get('/', function () {
-    return view('welcome');
-});
-*/
-
-Route::get('inicio', function () {
     return view('inicio');
 });
 
