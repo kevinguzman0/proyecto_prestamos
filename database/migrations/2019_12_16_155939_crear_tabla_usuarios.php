6b3d1cf13d89734e30029b5961d7a59d8747bf15
JhonJevii <58734526+JhonJevii@users.noreply.github.com>
2019-12-16 11:50:48 -0500

    my_migrate(jhon)

---
+++ database/migrations/2019_12_16_155939_crear_tabla_usuarios.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('idCliente', 11);
+            $table->integer('idPerfilUsuario', 2)->unsigned();
+            $table->foreign('idPerfilUsuario')->references('id')->on('perfiles_usuario');
+            $table->string('cedula', 15);
+            $table->string('nombres', 100);
+            $table->string('apellidos', 100);
+            $table->string('foto', 100);
+            $table->string('email', 100)->unique();
+            $table->string('telefono1', 15);
+            $table->string('telefono2', 15);
+            $table->date('fechaNacimiento');
+            $table->string('direccion', 100);
+            $table->string('barrio', 100);
+            $table->string('cuidad', 45);
+            $table->string('areaTrabajo', 100);
+            $table->string('cargoTrabajo', 100);           
+            $table->date('fechaCreacion');
+            $table->date('fechaModificacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
