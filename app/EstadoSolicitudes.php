cced319b5ca158d5ba1a628275803c973db7a18c
harango68 <53665166+harango68@users.noreply.github.com>
2019-12-16 14:44:36 -0500

    hernan 1444

---
+++ app/EstadoSolicitudes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EstadoSolicitudes extends Model
+{
+    protected $fillable=['nombreEstado', 'descripcionEstado'];
+    protected $table=['estado_solicitudes'];
+    protected $primarykey=['id'];
+}
