91f6c9df6ef153b99bcd60e3bf5295f4492e3989
kevinguzman0 <53454298+kevinguzman0@users.noreply.github.com>
2019-12-17 12:11:46 -0600

    css tabla

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,77 @@
 @section('tabla')
 	<style type="text/css">
 		.form{
-			background-color: black;
-			text-align: right;
-			width: 100%;
-			height: 300px;
+			width: 60%;
+			height: 250px;
+			margin-left: 600px;
 		}
+		.text{
+			background-color:
+			width: 50%;
+			margin: auto;
+		}
+
+		.valor{
+			margin-top: 40px;
+			border-radius: 10px;
+			border-style: solid;
+			border-color: #ff8c00;
+			border-width: 0.5px;
+			width: 30%;
+			height: 40px;
+			margin-left: 130px;
+			padding-left: 20px;
+		}
+		.valor:hover{
+			transition: 1s;
+			border-color: #00983b;
+		}
+		.plazo{
+			float: right;
+			margin-top: -55px;
+			border-radius: 10px;
+			border-style: solid;
+			border-color: #ff8c00;
+			border-width: 0.5px;
+			width: 30%;
+			height: 40px;
+			margin-right: 170px;
+			padding-left: 20px;
+		}
+		.plazo:hover{
+			transition: 1s;
+			border-color: #00983b;
+		}
+		.boton{
+			width: 80%;
+			margin: auto;
+		}
+
+
 	</style>
-	<div class="form">
+	<div class="form newsletter-area bg-img jarallax" style="background-image: url(prestamos2/img/bg-img/6.jpg);">
 		<form method="POST" action="{{ url ('/liquidador') }}">
 			@csrf
-		    <p>
-		    	<input type="text" name="valorDePrestamo" placeholder=" Valor del prestamo" class="valor">
-		    </p>
+			<div class="text">
 
-		    <p>
-		    	<input type="text" name="plazoEnCuotas" placeholder=" Plazo de cuotas">
-		    </p>
+				<p>
+		    	<input type="text" name="valorDePrestamo" placeholder="Valor del prestamo" class="valor">
+			    </p>
 
-		    <input type="submit" value="Calcular valor de la cuota" name="valorcuota">
+			    <p>
+			    	<input type="text" name="plazoEnCuotas" placeholder="Plazo de cuotas" class="plazo">
+			    </p>
 
-		    <input type="submit" value="Generar tabla de pagos" name="tablapagos">
+			</div>
+		    
+			<div class="boton">
 
+				<input type="submit" value="Calcular valor de la cuota" name="valorcuota" class="btn credit-btn mt-50 class_boton" style="width: 45%;">
+
+		    	<input type="submit" value="Generar tabla de pagos" name="tablapagos" class="btn credit-btn mt-50 class_boton" style="margin-left: 20px; width: 45%;">
+
+			</div>
+		    
 		</form>
 	</div>
 
