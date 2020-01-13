fb658616e94524deab6d244b6e23ed339c1e92dd
harango68 <53665166+harango68@users.noreply.github.com>
2020-01-13 14:14:03 -0500

    Revert "Update web.php"
    This reverts commit 163a31953f8b39c8df659104906a2bd781690d50.

--- routes/web.php
+++ routes/web.php
@@ -51,8 +51,8 @@ Route::post('validar_solicitud', 'ValidationSolicitudController@store')->middlew
 
 // -----------------------------------------------------------------------------------------------------------
 
-Route::get('mi-perfil', 'UsuarioController@index')->middleware('auth')->name('usuarios.perfil');
+Route::get('/mi-perfil', 'UsuarioController@index')->middleware('auth')->name('usuarios.perfil');
 
-Route::post('usuario-store', 'UsuarioController@store')->middleware('auth')->name('usuario.store');
+Route::post('', 'UsuarioController@store')->middleware('auth')->name('usuario.store');
 
 Route::get('docUsuarios/{filename}', 'UsuarioController@displayImage')->name('image.displayImage');
