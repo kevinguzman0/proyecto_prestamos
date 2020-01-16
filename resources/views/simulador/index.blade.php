3019d38484882c720cf0b085b538afaecb645f1c
harango68 <53665166+harango68@users.noreply.github.com>
2020-01-16 09:47:00 -0500

    ajustes

--- resources/views/simulador/index.blade.php
+++ resources/views/simulador/index.blade.php
@@ -38,12 +38,12 @@
 	            </div>
 
 	            @if ($errors->any())
-	                <div class="alert alert-danger mt-3 mb-1">
-	                    <ul>
+	                <div class="alert alert-danger mt-3 mb-1 margin-errors">
+	                    <ol class="estilo-lista-errores">
 	                        @foreach ($errors->all() as $error)
 	                            <li>{{ $error }}</li>
 	                        @endforeach
-	                    </ul>
+	                    </ol>
 	                </div>
 	            @endif
 
