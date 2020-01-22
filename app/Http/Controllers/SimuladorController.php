6cce6a4f5066661a833153111a6568b992a8bb1e
kevinguzman0 <53454298+kevinguzman0@users.noreply.github.com>
2020-01-22 11:47:47 -0600

    pdf

--- app/Http/Controllers/SimuladorController.php
+++ app/Http/Controllers/SimuladorController.php
@@ -133,6 +133,7 @@ class SimuladorController extends Controller
     public function vistaCuotaCredito(Request $request)
     {
 
+        Input::flash();
         $validatedData = Validator::make($request->all(),
             [
                 'valorPrestamo' => 'required|numeric',
