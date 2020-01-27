af8c9cb7be5e324bed6013d9dfcf35aa307e4ada
kevinguzman0 <53454298+kevinguzman0@users.noreply.github.com>
2020-01-27 09:18:23 -0600

    cd

--- database/migrations/2020_01_27_101627_create_notes_table.php
+++ database/migrations/2020_01_27_101627_create_notes_table.php
@@ -14,7 +14,9 @@ class CreateNotesTable extends Migration
     public function up()
     {
         Schema::create('notes', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
