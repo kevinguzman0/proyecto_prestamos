25c56ac7a93aeb4c53437cdd8cc9808cff9d6f1b
harango68 <53665166+harango68@users.noreply.github.com>
2020-01-30 11:39:47 -0500

    ajustes

--- resources/views/general/tablaUsuarios.blade.php
+++ resources/views/general/tablaUsuarios.blade.php
@@ -44,72 +44,205 @@
 	        </div>
 	    @endif
 
-    	{{ $usuarios->onEachSide(2)->links() }}
+ 	    @if($paginacion == 'si')
 
-		<table class="table table-striped table-bordered">
+    		{{ $usuarios->onEachSide(2)->links() }}
 
-			<tbody>
-				<thead class="header-tabla">
+		@endif
 
-					<tr>
-						<th class="header-tabla-texto">Id</th>
-						<th class="header-tabla-texto">Usuario</th>
-						<th class="header-tabla-texto">Email</th>
-						<th class="header-tabla-texto">Acciones</th>
-					</tr>
+		<p>
+			<button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#seccionFiltros" aria-expanded="false" aria-controls="collapseExample">
+				Ver filtros disponibles
+			</button>
+		</p>
 
-				</thead>
+        <div class="form-row col-md-12 padding-form">		
 
-				@foreach ($usuarios as $fila)
+			<div class="collapse col-md-12 mb-3" id="seccionFiltros">
 
-					@if(!$fila->hasAnyRole('administrador'))
+				<div class="card card-body">
 
-					    <tr>
+					<form class="col-md-12 padding-form" method="POST">
 
-							<td style="text-align:center; font-weight: bold;"> {{ $fila->id }} </td>
-							<td style="text-align:left;"> {{ $fila->name }} </td>
-							<td style="text-align:left;"> {{ $fila->email }} </td>
+			            @csrf
 
-							<td style="text-align:left;">
+			            <div class="form-row col-md-12 padding-form">
+			            	
+						    <div class="input-group col-md-10">
 
-								@include('modals.datos-usuarios')
-								
-								@hasanyrole('directivo')
+						    	<input type="text" name="filtro" class="form-control" placeholder="escriba texto a buscar...">
 
-									@if(Auth::user()->id != $fila->id)
+						  		<div class="input-group-append">
 
-										@include('modals.eliminar-usuarios')
+							      	<button class="btn btn-dark" type="submit" name="btnBuscar"  formaction="{{ action('GeneralController@buscadorUsuarios') }}">
 
+							        	Buscar
+
+							      	</button>
+
+							    </div>
+
+						    </div>
+
+						    <div class="col-md-2">
+
+						      	<input type="submit" value="Mostrar todos" name="btnMostrarTodos" formaction="{{ action('GeneralController@todosUsuarios') }}" class="form-control btn btn-success">
+
+						    </div>
+
+			            </div>
+
+					</form>
+
+					<form class="col-md-12 padding-form" method="POST">
+
+			            @csrf
+
+					    <div class="form-row col-md-12 padding-form">
+
+						  	@isset($idUsuarios)
+
+							    <div class="col-md-2">
+
+							    	<label class="label-margin">Id Usuario</label>
+							      	<select class="form-control" id="cboIdUsuarios" name="cboIdUsuarios">
+							      	
+								      	<option value="-1">Todos</option>
+								      	@foreach ($idUsuarios as $item)
+									        <option value="{{ $item->id }}">{{ $item->id }}</option>
+									    @endforeach
+
+							      	</select>
+
+							    </div>
+
+						    @endisset
+
+						    <div class="col-md-2">
+
+						    	<label class="label-margin">Email verificado</label>
+						      	<select class="form-control" id="verificacionEmail" name="verificacionEmail">
+						      	
+							      	<option value="-1">Todos</option>
+							      	<option value="1">Si</option>
+							      	<option value="0">No</option>
+
+						      	</select>
+
+						    </div>
+
+						</div>
+
+					    <div class="form-row col-md-12 padding-form">
+
+						    <div class="col-md-2">
+
+						    	<label class="label-margin">Fecha de</label>
+						      	<select class="form-control" id="cboFechaDe" name="cboFechaDe">
+						      	
+							      	<option value="created_at">Creación</option>
+							      	<option value="updated_at">Modificación</option>
+							      	<option value="email_verified_at">Verificación</option>
+
+						      	</select>
+
+						    </div>
+
+			                <div class="col-md-3">
+			                    <label class="label-margin">Fecha inicial</label>
+			                    <input type="date" name="fechaInicial" class="form-control">
+			                </div>
+
+			                <div class="col-md-3">
+			                    <label class="label-margin">Fecha Final</label>
+			                    <input type="date" name="fechaFinal" class="form-control">
+			                </div>
+
+							<div class="col-md-2 ml-auto">
+
+					      		<label class="label-margin"></label>
+					      		<input type="submit" value="Filtrar" name="btnFiltrar" formaction="{{ action('GeneralController@filtrosUsuarios') }}" class="form-control btn btn-dark boton-filtrar">
+
+					      	</div>
+
+						</div>
+					
+					</form>	
+
+				</div>
+
+			</div>
+
+        </div>
+
+		@isset($usuarios)
+
+			<table class="table table-striped table-bordered">
+
+				<tbody>
+					<thead class="header-tabla">
+
+						<tr>
+							<th class="header-tabla-texto">Id</th>
+							<th class="header-tabla-texto">Usuario</th>
+							<th class="header-tabla-texto">Email</th>
+							<th class="header-tabla-texto">Acciones</th>
+						</tr>
+
+					</thead>
+
+					@foreach ($usuarios as $fila)
+
+						@if(!$fila->hasAnyRole('administrador'))
+
+						    <tr>
+
+								<td style="text-align:center; font-weight: bold;"> {{ $fila->id }} </td>
+								<td style="text-align:left;"> {{ $fila->name }} </td>
+								<td style="text-align:left;"> {{ $fila->email }} </td>
+
+								<td style="text-align:left;">
+
+									@include('modals.datos-usuarios')
+									
+									@hasanyrole('directivo')
+
+										@if(Auth::user()->id != $fila->id)
+
+											@include('modals.eliminar-usuarios')
+
+										@endif
+
+									@endhasanyrole
+
+									@if (!Auth::user()->hasAnyRole('administrador'))
+										<a href="{{ action('PerfilController@miPerfil', [$fila->id]) }}">
+											@if(App\User::find($fila->id)->perfil != null)
+												<img src="{{ asset('icons/person-fill.svg') }}" alt="Ver perfil" width="36" height="36" title="Ver perfil">
+											@else
+												<img src="{{ asset('icons/camera.svg') }}" alt="Crear perfil" width="36" height="36" title="Crear perfil">
+											@endif
+										</a>
+									@endif
+									
+									@if($fila->email_verified_at == null)
+										<a href="{{ route('usuario.validar', [$fila->id]) }}">
+											<img src="{{ asset('icons/unlock.svg') }}" alt="Validar email / Desbloquear cuenta" width="36" height="36" title="Validar email / Desbloquear cuenta">
+										</a>
 									@endif
 
-								@endhasanyrole
+								</td>						
 
-								@if (!Auth::user()->hasAnyRole('administrador'))
-									<a href="{{ action('PerfilController@miPerfil', [$fila->id]) }}">
-										@if(App\User::find($fila->id)->perfil != null)
-											<img src="{{ asset('icons/person-fill.svg') }}" alt="Ver perfil" width="36" height="36" title="Ver perfil">
-										@else
-											<img src="{{ asset('icons/camera.svg') }}" alt="Crear perfil" width="36" height="36" title="Crear perfil">
-										@endif
-									</a>
-								@endif
-								
-								@if($fila->email_verified_at == null)
-									<a href="{{ route('usuario.validar', [$fila->id]) }}">
-										<img src="{{ asset('icons/unlock.svg') }}" alt="Validar email / Desbloquear cuenta" width="36" height="36" title="Validar email / Desbloquear cuenta">
-									</a>
-								@endif
+							</tr>
+						@endif
 
-							</td>						
+					@endforeach
 
-						</tr>
-					@endif
+				</tbody>
 
-				@endforeach
+			</table>
 
-			</tbody>
-
-		</table>
+		@endisset
 
 	</div>
 
