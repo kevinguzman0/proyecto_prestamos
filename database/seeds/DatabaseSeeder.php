33be63373c50739f212640c0d1de0f307ff74c96
kevinguzman0 <53454298+kevinguzman0@users.noreply.github.com>
2020-01-31 13:38:13 -0600

    dsa

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         $this->call(SpatieSeeder::class);
 
         $faker = Faker::create();
-        //$faker->addProvider(new Faker\Provider\en_US\Person($faker));
+        $idCliente = random_int(\DB::table('users')->min('id'), \DB::table('users')->max('id'));
+        $idEstadoSolicitud=random_int(\DB::table('estados_solicitud')->min('id'), \DB::table('estados_solicitud')->max('id'));
 
         foreach (range(1,20) as $index) 
         {
@@ -34,6 +35,19 @@ class DatabaseSeeder extends Seeder
 
             $usuario->assignRole('registrado');
 
+        }
+
+        foreach (range(1,50) as $solicitudes) {
+             DB::table('solicitudes')->insert([
+                'idCliente' => $idCliente,
+                'idEstadoSolicitud' => $idEstadoSolicitud,
+                'monto' => $faker->numberBetween(1000, 72000000),
+                'plazo' => $faker->numberBetween(1, 48),
+                'cuota' => $faker->randomFloat(2, 1, 100),
+                'interes' => $faker->randomFloat(2, 1, 100),
+                'idAnalizadoPor' => $idCliente,
+                'analizadoEn' => $faker->optional()->dateTimeInInterval($startDate = '-1 years', $interval = '+ 5 days', $timezone = 'America/Bogota'),
+            ]);
         }
 
         /*
@@ -65,36 +79,6 @@ class DatabaseSeeder extends Seeder
         
         */
 
-        /*
-
-    	$faker = Faker::create('es_ES');
-        $idCliente = random_int(\DB::table('users')->min('id'), \DB::table('users')->max('id'));
-        $idEstadoSolicitud=random_int(\DB::table('estados_solicitud')->min('id'), \DB::table('estados_solicitud')->max('id'));
-
-    	foreach (range(1,20) as $index) {
-	        DB::table('users')->insert([
-	            'name' => $faker->name,
-	            'email' => $faker->email,
-	            'email_verified_at' => $faker->optional()->dateTimeInInterval($startDate = '-1 years', $interval = '+ 5 days', $timezone = 'America/Bogota'),
-	            'password' => bcrypt('secret'),
-	            'remember_token' => str_random(10),
-	        ]);
-    	}
-
-        foreach (range(1,50) as $solicitudes) {
-             DB::table('solicitudes')->insert([
-                'idCliente' => $idCliente,
-                'idEstadoSolicitud' => $idEstadoSolicitud,
-                'monto' => $faker->numberBetween(1000, 72000000),
-                'plazo' => $faker->numberBetween(1, 48),
-                'cuota' => $faker->randomFloat(2, 1, 100),
-                'interes' => $faker->randomFloat(2, 1, 100),
-                'idAnalizadoPor' => $idCliente,
-                'analizadoEn' => $faker->optional()->dateTimeInInterval($startDate = '-1 years', $interval = '+ 5 days', $timezone = 'America/Bogota'),
-            ]);
-        }
-
-        */
 	}
 
 }
