8569d9f58855a0b7bba55299a438f41eb9950b39
JhonJevii <58734526+JhonJevii@users.noreply.github.com>
2020-01-31 11:47:54 -0500

    we

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,9 @@ class DatabaseSeeder extends Seeder
     {
     	$this->call(SpatieSeeder::class);
 
-    	$faker = Faker::create();
+    	$faker = Faker::create('es_ES');
+        //$idCliente=DB::table('users')->pluck('id')->toArray();;
+        //$idEstadoSolicitud=DB::table('estados_solicitud')->pluck('id')->toArray();;
 
     	foreach (range(1,10) as $index) {
 	        DB::table('users')->insert([
@@ -29,7 +31,18 @@ class DatabaseSeeder extends Seeder
 	            'remember_token' => str_random(10),
 	        ]);
     	}
-	}
 
-    }
+        foreach (range(1,10) as $solicitudes) {
+             DB::table('solicitudes')->insert([
+                'idCliente' => random_int(\DB::table('users')->min('id'), \DB::table('users')->max('id')),
+                'idEstadoSolicitud' => random_int(\DB::table('estados_solicitud')->min('id'), \DB::table('estados_solicitud')->max('id')),
+                'monto' => $faker->numberBetween(1000, 72000000),
+                'plazo' => $faker->numberBetween(1, 48),
+                'cuota' => $faker->randomFloat(2, 1, 100),
+                'interes' => $faker->randomFloat(2, 1, 100),
+                'idAnalizadoPor' =>random_int(\DB::table('users')->min('id'), \DB::table('users')->max('id')),
+                'analizadoEn' => $faker->optional()->dateTimeInInterval($startDate = '-1 years', $interval = '+ 5 days', $timezone = 'America/Bogota'),
+            ]);
+        }
+	}
 }
